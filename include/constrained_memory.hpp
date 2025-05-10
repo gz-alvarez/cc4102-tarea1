1ad1c9309c0007b2424ae95a67d603ea476deee1
gz-alvarez <56127864+gz-alvarez@users.noreply.github.com>
2025-05-10 10:45:54 -0400

    add variable for M

---
+++ include/constrained_memory.hpp
@@ -0,0 +1,9 @@
+#ifndef CONSTRAINED_MEMORY_HPP
+#define CONSTRAINED_MEMORY_HPP
+
+
+constexpr unsigned int M_megabytes = 50.0; // tamaño de memoria principal
+constexpr unsigned int M_bytes = M_megabytes * 1'000'000;
+
+
+#endif
